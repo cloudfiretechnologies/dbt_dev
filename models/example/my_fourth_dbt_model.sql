0ba0270801a2c67b543d3396e1f7eec7694ce737
snowflakeacctest@gmail.com <snowflakeacctest@gmail.com>
2024-03-24 02:51:53 +0000

    added new changes

--- models/example/my_fourth_dbt_model.sql
+++ models/example/my_fourth_dbt_model.sql
@@ -7,7 +7,8 @@
 
 
 with temp_tbl as (
-select * from DBT_TEST.DBT_SCHEMA.COUNTRY limit 50
+(select * from DBT_TEST.DBT_SCHEMA.COUNTRY limit 50)
 union all
-select * from {{ ref('my_third_dbt_model') }} )
+(select * from {{ ref('my_third_dbt_model') }})
+)
 select * from temp_tbl
